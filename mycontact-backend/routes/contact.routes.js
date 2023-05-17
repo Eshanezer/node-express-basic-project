ecd12c3bd7aaaa191ac5cbe1c8a7b417cc4e618f
Eshan Dilmina <106302589+eshanceylonsoft@users.noreply.github.com>
2023-05-17 21:21:09 +0530

    another route method

--- mycontact-backend/routes/contact.routes.js
+++ mycontact-backend/routes/contact.routes.js
@@ -1,23 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {
-    getContacts,
-    createContact,
-    updateContact,
-    deleteContact,
-    getByIdContact
-} = require('../controllers/contacts.controller');
-
+  getContacts,
+  createContact,
+  updateContact,
+  deleteContact,
+  getByIdContact,
+} = require("../controllers/contacts.controller");
 
 // router.route('/').get((request,response)=>{
 //     response.status(200).json({message:'GET ALL CONTACTS'});
 // });
 
-router.get('/',getContacts);
-router.post('/',createContact);
-router.get('/:id',getByIdContact);
-router.put('/:id',updateContact);
-router.delete('/:id',deleteContact);
+router.route('/').get(getContacts).post(createContact);
+router.post("/", createContact);
+router.get("/:id", getByIdContact);
+router.put("/:id", updateContact);
+router.delete("/:id", deleteContact);
 
-
-module.exports=router;
+module.exports = router;
